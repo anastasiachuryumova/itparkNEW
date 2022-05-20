2174b090c23d968153cd0b37bbb96d4a322d2fd9
anastasiachuryumova <93443434+anastasiachuryumova@users.noreply.github.com>
2022-05-20 18:59:46 +0300

    Delete itpark directory

--- itpark/newsaggregator/src/main/java/newsaggregator/mapper/NewsMapper.java
+++
@@ -1,7 +0,0 @@
-package newsaggregator.mapper;
-
-import org.mapstruct.Mapper;
-
-@Mapper
-public interface NewsMapper extends AbstractMapper<newsaggregator.model.sql.News, newsaggregator.model.nosql.News>{
-}
--- itpark/newsaggregator/src/main/resources/db/migrations/news-data.sql
+++
@@ -1,6 +0,0 @@
--- Заполняем таблицу новостей
-create table NEWS
-(
-    id    long not null,
-    title varchar(255) not null
-);
